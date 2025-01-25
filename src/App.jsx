368f350842b4383fb3ccde3689f3e70b79ad1902
= <dinaolmelak@gmail.com>
2025-01-25 13:14:37 -0600

    Feature: Added fields and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [expense, setExpense] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  const [expenses, setExpenses] = useState([]);
+
+  function addExpense() {
+    if(!expense || !amount || !date || isNaN(amount)){
+      alert("Please fill out all input.");
+      return;
+    }
+    console.log("Add expense");
+    const newExpense = {
+      id: Date.now(), 
+      expense, 
+      amount: parseFloat(amount), 
+      date: new Date(date)
+    };
+    setExpenses([...expenses, newExpense]);
+    setExpense('');
+    setAmount('');
+    setDate('');
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <label>Expenses</label>
+          {
+            expenses.map((item)=>(
+              <li key={item.id}>
+                {item.expense} - ${item.amount} - {item.date.toLocaleDateString()}
+              </li>
+            ))
+          }
+        </div>
+        <input placeholder='Expense' required type='text' value={expense} 
+          onChange={(e) => setExpense(e.target.value)}></input><br/>
+        <input placeholder='Amount' required type='number' value={amount}
+          onChange={(e) => setAmount(e.target.value)}></input><br/>
+        <input placeholder='Date' required type='date' value={date}
+          onChange={(e) => setDate(e.target.value)}></input><br/>
+
+        <button onClick={addExpense}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button>Expenses</button>
+      <button>Visualize</button>
+    </> 
   )
 }
 
